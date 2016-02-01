4daf71c8b8279c5e061c15c1e61cfe68f64501d3
Joe Pinsonault <joe.pinsonault@gmail.com>
2016-01-31 16:11:16 -0800

    First commit

---
+++ src/ServiceManager.java
@@ -0,0 +1,12 @@
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+
+public class ServiceManager {
+
+    public void startServices(ArrayList<String> classList) {
+        Gson g = new Gson();
+
+
+    }
+}
